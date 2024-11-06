e22dfb31053fa499e5a6a51e8039c7382a4e4a9d
bhagyeshsp <bhagyeshspathak@gmail.com>
2024-11-06 12:55:37 +0530

    Add pseudocode for the first leg of work

---
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -0,0 +1,20 @@
+// Computer's choice
+// Generate random number first
+// Based on the random number, assign either of the three choices: rock, paper or scissors
+// Store that choice in a variable as computer's choice
+
+// Human's choice
+// Ask for human's choice: rock, paper or scissors
+// Assign that value to a variable as human's choice
+
+// Setup score trackers
+// Create a computer score variable and initialize it with 0
+// Create a human score variable and initialize it with 0
+
+// Game playing
+// Create a function that takes two arguments: computer's choice and human's choice
+// This function compares both and addresses the user by saying something like: "You lose! Paper beats Rock!"
+
+// Score increment
+// If the computer choice beat human choice, increment the computer score by 1
+// If the human choice beat computer choice, increment the human score by 1
